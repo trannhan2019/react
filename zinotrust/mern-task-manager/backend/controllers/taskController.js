4cbcb9c781205c26e40b516ab28de5d0d0cc4ebb
TranNhan <trannhan.s3@gmail.com>
2022-10-24 21:54:20 +0700

    zinotrust-taskmanager-finish 3

--- zinotrust/mern-task-manager/backend/controllers/taskController.js
+++ zinotrust/mern-task-manager/backend/controllers/taskController.js
@@ -45,9 +45,26 @@ const deleteTask = async (req, res) => {
   }
 };
 
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json(`No task with id : ${id}`);
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 module.exports = {
   getTasks,
   createTask,
   getTask,
   deleteTask,
+  updateTask,
 };
--- zinotrust/mern-task-manager/backend/routes/taskRoute.js
+++ zinotrust/mern-task-manager/backend/routes/taskRoute.js
@@ -6,15 +6,16 @@ const {
   createTask,
   getTask,
   deleteTask,
+  updateTask,
 } = require('../controllers/taskController');
 
-router.get('/', getTasks);
-router.post('/', createTask);
-router.get('/:id', getTask);
-router.delete('/:id', deleteTask);
-// router.put("/:id", updateTask);
+// router.get('/', getTasks);
+// router.post('/', createTask);
+// router.get('/:id', getTask);
+// router.delete('/:id', deleteTask);
+// router.put('/:id', updateTask);
 
-// router.route("/").get(getTasks).post(createTask);
-// router.route("/:id").get(getTask).put(updateTask).delete(deleteTask);
+router.route('/').get(getTasks).post(createTask);
+router.route('/:id').get(getTask).put(updateTask).delete(deleteTask);
 
 module.exports = router;
