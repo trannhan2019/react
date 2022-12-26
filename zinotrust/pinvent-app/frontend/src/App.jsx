d2a98b6c302740d6dbf489f43e07f08267266d77
TranNhan <trannhan.s3@gmail.com>
2022-12-26 23:17:33 +0700

    zinotrust-pinvent-frontend-register

--- zinotrust/pinvent-app/frontend/src/App.jsx
+++ zinotrust/pinvent-app/frontend/src/App.jsx
@@ -1,14 +1,35 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import Register from './pages/auth/Register';
 import Home from './pages/home/Home';
+import { SET_LOGIN } from './redux/features/auth/authSlice';
+import { getLoginStatus } from './services/authService';
+
+axios.defaults.withCredentials = true;
+export const SERVER_URL = 'http://localhost:5000';
 
 function App() {
+  const dispatch = useDispatch();
+  //check if user is logged in
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus();
+      dispatch(SET_LOGIN(status));
+    }
+    loginStatus();
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ zinotrust/pinvent-app/frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+
+import './Loader.scss';
+import loaderImg from '../../assets/loader.gif';
+
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className={'wrapper'}>
+      <div className={'loader'}>
+        <img src={loaderImg} alt="Loading..." />
+      </div>
+    </div>,
+    document.getElementById('loader')
+  );
+};
+
+export const SpinnerImg = () => {
+  return (
+    <div className="--center-all">
+      <img src={loaderImg} alt="Loading..." />
+    </div>
+  );
+};
+
+export default Loader;
