aa86b84c22f879f1cb6376e60c03c96ad3395587
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2022-12-30 14:03:14 +0700

    finish front-end dashboard page

--- zinotrust/pinvent-app/backend/server.js
+++ zinotrust/pinvent-app/backend/server.js
@@ -1,14 +1,15 @@
-const dotenv = require('dotenv').config();
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRoute = require('./routes/userRoute');
-
-const errorHandler = require('./middleware/errorMiddleware');
-
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+const path = require("path");
 // Import cookie parser
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -18,31 +19,37 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Fix Cors
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Fix Cors
 app.use(
   cors({
-    origin: [`${process.env.CLIENT_URL}`],
+    origin: ["http://localhost:5173", "https://pinvent.vercel.app"],
     credentials: true,
   })
 );
+// Save cookie from server proxy
+// app.set("trust proxy", 1);
 
 // Routes Middleware
-app.use('/api/users', userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/", contactRoute);
 
-//Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page');
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page");
 });
 
 // errorHandler Should be the last middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+// Connect DB & start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}...`)
-    )
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}...`);
+    })
   )
   .catch((err) => console.log(err));
--- zinotrust/pinvent-app/frontend/src/redux/store.js
+++ zinotrust/pinvent-app/frontend/src/redux/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/auth/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth/authSlice";
+import productReducer from "./features/product/productSlice";
+import filterReducer from "../redux/features/productFilter/filterSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    product: productReducer,
+    filter: filterReducer,
   },
 });
