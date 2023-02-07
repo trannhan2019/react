77596c433c1e0ead3787a82da582b96c3b29b450
Tran Nhan Home <trannhan.s3@gmail.com>
2023-02-08 06:30:42 +0700

    07.02.2023

--- trannhan/first-project/mui-client/src/pages/SignInPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SignInPage() {
-  return (
-    <div>SignInPage</div>
-  )
-}
--- trannhan/hip_phong_tro/server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoute from "./auth.route";
-
-const router = express.Router();
-
-router.use("/", authRoute);
-
-export default router;
