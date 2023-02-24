30d81df94a6136636bc236026ce01f071eb3da32
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2023-02-24 15:06:54 +0700

    24.02.2023 del tailwind

--- trannhan/tailwind-ui/src/components/blog/button/BtnSimple.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BtnSimple({ children, className = "", ...props }) {
-  return (
-    <button className={`rounded-2xl ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
